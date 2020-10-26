b80953b452dc9a8709f865cccbfe6af9522a6265
SC-Account <tillhen2@gmail.com>
2020-10-26 21:04:53 +0100

    run primary project with maximum buildpower (or 50% of total requested eco)

--- modules/throttler/MassPlugin.lua
+++ modules/throttler/MassPlugin.lua
@@ -1,14 +1,19 @@
 local modPath = '/mods/EM/'
 local ThrottlerPlugin = import(modPath .. 'modules/throttler/ThrottlerPlugin.lua').ThrottlerPlugin
 
+--todo:
+--add pause before engies reach mex
+--run primary project with maximum buildpower (or 20% of total requested eco)
+
 MassPlugin = Class(ThrottlerPlugin) {
 	constructionCategories = {
 		--{name="Mass Extractors T1", category = categories.STRUCTURE * categories.TECH1 * categories.MASSEXTRACTION, priority = 90},
-		{name="Mass Extractors T2", category = categories.STRUCTURE * categories.TECH2 * categories.MASSEXTRACTION, priority = 90, storage = 0.01},
-		{name="Mass Storage", category = categories.STRUCTURE * categories.MASSSTORAGE, priority = 90, storage = 0.01},
-		{name="Mass Extractors T3", category = categories.STRUCTURE * categories.TECH3 * categories.MASSEXTRACTION, priority = 90, storage = 0.01},
+		{name="Mass Extractors T2", category = categories.STRUCTURE * categories.TECH2 * categories.MASSEXTRACTION, priority = 90, storage = 0.01, massProduction = true},
+		{name="Mass Storage", category = categories.STRUCTURE * categories.MASSSTORAGE, priority = 90, storage = 0.01, massProduction = true},
+		{name="Mass Extractors T3", category = categories.STRUCTURE * categories.TECH3 * categories.MASSEXTRACTION, priority = 90, storage = 0.01, massProduction = true},
 	},
-	
+	MassProductionRequestedMass = 0,
+
 	_sortProjects = function(a, b)
 
 		--handles buildables
@@ -39,6 +44,7 @@ MassPlugin = Class(ThrottlerPlugin) {
 		end
 
 		if category then
+			project.isMassProduction = category['massProduction']
 			project.prio = category['priority']
 			project.massMinStorage = category['storage']
 			table.insert(self.projects, project)
@@ -49,10 +55,17 @@ MassPlugin = Class(ThrottlerPlugin) {
 		local net = eco:massNet(0)
 		local new_net
 
-		local new_net = net - math.min(project.massRequested, project.massCostRemaining)
-		if new_net < 0 then -- next project will stall eco
-			-- allow stall untill stalling 50% of eco:
+		if project.isMassProduction then 
+			self.MassProductionRequestedMass = self.MassProductionRequestedMass + math.min(project.massRequested, project.massCostRemaining)
+		end
+
+		local new_net = net - math.min(project.massRequested, project.massCostRemaining) + (eco.massRequested * 0.5 - self.MassProductionRequestedMass) -- allow stall untill stalling 50% of eco except that used by mass prod
+		if new_net < 0 then -- this project will stall eco
 			project:SetMassDrain(math.max(0, net))
 		end
+	end,
+	resetCycle = function(self)
+		self.projects = {}
+		self.MassProductionRequestedMass = 0
 	end,
 }
