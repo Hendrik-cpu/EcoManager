0ac401680399ae6f86221df677a5f0930cf89909
SC-Account <tillhen2@gmail.com>
2021-02-13 06:26:08 +0100

    more aggressive ethrottle

--- modules/throttler/Economy.lua
+++ modules/throttler/Economy.lua
@@ -134,6 +134,6 @@ Economy = Class({
 		else
 			energyMin = math.max(self['energyMinStored'],energyMin)
 		end
-		return self:net('energy', energyMin, 0.5)
+		return self:net('energy', energyMin, 0.1)
 	end,
 })
